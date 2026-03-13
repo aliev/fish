9494c56cb6b110221bf6d11c953209cabd10e974
Ali Aliyev <ali@aliev.me>
2026-03-13 23:35:59 +0400

    Add fzf theme and default options

---
+++ conf.d/fzf.fish
@@ -0,0 +1,2 @@
+set -x FZF_DEFAULT_COMMAND 'rg --files --hidden'
+set -x FZF_DEFAULT_OPTS '--height=50% --layout=reverse --border --color=fg:#d4d4d4,bg:#1e1e1e,hl:#569cd6,fg+:#ffffff,bg+:#264f78,hl+:#4ec9b0,info:#608b4e,prompt:#569cd6,pointer:#c586c0,marker:#6a9955,spinner:#569cd6,header:#569cd6,border:#404040'
