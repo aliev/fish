5a063ffd755283576921253d0442689202de7567
Ali Aliyev <ali@aliev.me>
2026-03-14 12:26:23 +0400

    chore: remove unused fe (fzf file edit) function

--- functions/fe.fish
+++
@@ -1,6 +0,0 @@
-function fe -d "Browse and edit files with fzf"
-    set -l files (git ls-files --cached --others --exclude-standard 2>/dev/null; or find . -type f)
-    printf '%s\n' $files | fzf --ansi \
-        --preview "cat -n {}" \
-        --bind "enter:execute($EDITOR {})"
-end
